009de261701d4bd49d90839a9c8e2ad4b7754de0
shahi5472 <s.m.kamalhussain@gmail.com>
2020-04-03 00:02:24 +0600

    learn about phptest/nothing learn, but understand

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-//    return view('welcome');
+    return view('welcome');
 
 //    $user = factory(\App\User::class)->create();
 //    $phone = new \App\Phone();
